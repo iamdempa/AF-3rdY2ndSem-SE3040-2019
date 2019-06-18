7bcd1e15ff5b6004b434060d9a86f70421c5e900
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-06-18 22:38:24 +0530

    Added Server.js, App.jsx etc...

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const server = express();
+const bodyParser = require("body-parser");
+const Bundler = require("parcel-bundler");
+const cors = require("cors");
+
+const bundler = new Bundler('./src/index.html');
+
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+server.use(bundler.middleware());
+server.use(express.static('./dist'));
+
+server.get('/', function (req, res) {
+    res.sendFile('./dist/index.html');
+});
+
+server.listen(3000, err => {
+    if (err) {
+        console.error(err);
+        process.exit(-1)
+    }
+    console.log('Application is running on port 3000');
+});
+
---
+++ src/AppComponent.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class AppComponent extends Component{
+
+    render() {
+        return <div>
+            <h1>Hello</h1>
+        </div>
+    }
+}
